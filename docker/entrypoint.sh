0d460f9868de1d467a96a1536daab5199af2bc3c
jesanchez1 <jesanchez@virfon.com>
2025-10-28 22:47:58 -0400

    added test for cards, modified entrypoint to run these at start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,6 +9,9 @@ python app/scripts/setup_db.py
 echo "Seeding test users..."
 python app/scripts/test_seed_users.py
 
+echo "Running application tests..."
+python -m app.tests.test_cards_routes
+
 
 echo "Starting application..."
 exec "$@"
